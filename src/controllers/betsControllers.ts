68b7a394813b777f11037f7b776d820cc49f4b30
gabriel-victor933 <gabrielvictor933@usp.br>
2023-10-10 19:33:05 -0300

    fixing missing throw pattern

--- src/controllers/betsControllers.ts
+++ src/controllers/betsControllers.ts
@@ -6,5 +6,4 @@ export async function postBet(req: Request, res: Response) {
     const body = req.body as createBet
     await betsServices.postBet(body)
     return res.sendStatus(201)
-
 }
--- src/controllers/participantsControllers.ts
+++ src/controllers/participantsControllers.ts
@@ -1,17 +1,17 @@
 import {Request, Response} from "express"
 import { createParticipant } from "../utils/protocols"
 import participantServices from "../services/participantsService"
+import { invalidRequestException } from "utils/errors"
 
 export async function postParticipants(req: Request, res: Response) {
     const infos = req.body as createParticipant
-    if (infos.balance < 1000) return res.status(400).send("invalid balance!")
+    if (infos.balance < 1000) throw invalidRequestException("insufficient Balance!")
     await participantServices.postParticipant(infos)
     return res.sendStatus(201)
 
 }
 
 export async function getParticipants(req: Request, res: Response) {
-
     const part = await participantServices.getParticipants()
     return res.send(part)
 
